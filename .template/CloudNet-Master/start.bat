ff982bddb61c24b597186e7f169f13211cc12e61
Marvin <marv.1997@live.de>
2018-06-21 18:13:39 +0200

    Add template for release file

---
+++ .template/CloudNet-Master/start.bat
@@ -0,0 +1,1 @@
+java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:MaxPermSize=256M -Xmx128m -jar CloudNet-Master.jar
---
+++ .template/CloudNet-Wrapper/start.sh
@@ -0,0 +1,1 @@
+screen -S Wrapper-1 java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:MaxPermSize=256M -Xmx64m -jar CloudNet-Wrapper.jar
