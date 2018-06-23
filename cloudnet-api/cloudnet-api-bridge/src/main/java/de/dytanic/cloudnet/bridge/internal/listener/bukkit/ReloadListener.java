cba6b5c1399a7899106aaf563ef869ee6c541cb4
Sarsum <33085157+Sarsum@users.noreply.github.com>
2018-06-23 22:07:52 +0200

    Cleaned up Listener Class for blocking the reload command

--- cloudnet-api/cloudnet-api-bridge/src/main/java/de/dytanic/cloudnet/bridge/internal/listener/bukkit/ReloadListener.java
+++ cloudnet-api/cloudnet-api-bridge/src/main/java/de/dytanic/cloudnet/bridge/internal/listener/bukkit/ReloadListener.java
@@ -7,19 +7,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class ReloadListener implements Listener {
-    private final String[] blockedCmd = {"/reload", "/rl", "/bukkit:rl", "/bukkit:reload"};
+    private static final String[] blockedCommands = {"/reload", "/rl", "/bukkit:rl", "/bukkit:reload"};
 
     @EventHandler
     public void onDispatch(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
         if (isCmdBlocked(event.getMessage())) {
-            player.sendMessage(CloudAPI.getInstance().getPrefix() + "The command was blocked for bug avoidance");
+            player.sendMessage(CloudAPI.getInstance().getPrefix() + "This command has been blocked because it leads to problems using CloudNet.");
             event.setCancelled(true);
         }
     }
 
     private boolean isCmdBlocked(String commandLine) {
-        for (String blockedCmd : blockedCmd) {
+        for (String blockedCmd : blockedCommands) {
             if (commandLine.toLowerCase().startsWith(blockedCmd)) {
                 return true;
             }
