bc241648bbd215343a30ab57bc31b7683909c875
Sarsum <33085157+Sarsum@users.noreply.github.com>
2018-07-05 19:27:51 +0200

    The reload command is now also blocked in the console

--- cloudnet-api/cloudnet-api-bridge/src/main/java/de/dytanic/cloudnet/bridge/internal/listener/bukkit/ReloadListener.java
+++ cloudnet-api/cloudnet-api-bridge/src/main/java/de/dytanic/cloudnet/bridge/internal/listener/bukkit/ReloadListener.java
@@ -1,10 +1,12 @@
 package de.dytanic.cloudnet.bridge.internal.listener.bukkit;
 
 import de.dytanic.cloudnet.api.CloudAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 
 public class ReloadListener implements Listener {
     private static final String[] blockedCommands = {"/reload", "/rl", "/bukkit:rl", "/bukkit:reload"};
@@ -15,6 +17,14 @@ public class ReloadListener implements Listener {
         if (isCmdBlocked(event.getMessage())) {
             player.sendMessage(CloudAPI.getInstance().getPrefix() + "This command has been blocked because it leads to problems using CloudNet.");
             event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void handle(ServerCommandEvent event) {
+        if (isCmdBlocked(event.getCommand())) {
+            event.setCancelled(true);
+            Bukkit.getConsoleSender().sendMessage("This command has been blocked because it leads to problems using CloudNet.");
         }
     }
 
