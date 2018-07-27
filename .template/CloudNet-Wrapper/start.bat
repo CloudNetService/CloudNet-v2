b2faa560ed2bb93e59be5091fffc373c818201f1
Dytanic <tarekhosni1410@hotmail.com>
2018-07-28 01:02:07 +0200

    add default --ssl argument + add jvm options

--- .template/CloudNet-Wrapper/start.bat
+++ .template/CloudNet-Wrapper/start.bat
@@ -1,1 +1,1 @@
-java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:MaxPermSize=256M -Xmx128m -jar CloudNet-Wrapper.jar
+java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:MaxPermSize=256M  -XX:+OptimizeStringConcat -XX:CompileThreshold=100 -Xmx128m -jar CloudNet-Wrapper.jar --ssl
