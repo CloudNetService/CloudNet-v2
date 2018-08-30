c82c46853fd2d499ee9434eb1bf2c9d810807a08
_Klaro <pasqual.koschmieder@gmail.com>
2018-08-30 10:50:57 +0200

    Fixed WebClient ToString

--- cloudnet-cord/cloudnet-web/src/main/java/de/dytanic/cloudnet/web/client/WebClient.java
+++ cloudnet-cord/cloudnet-web/src/main/java/de/dytanic/cloudnet/web/client/WebClient.java
@@ -96,8 +96,9 @@ public class WebClient {
     {
         try
         {
-            URLConnection urlConnection = new java.net.URL(url).openConnection();
+            URLConnection urlConnection = new URL(url).openConnection();
             urlConnection.setUseCaches(false);
+            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             urlConnection.setConnectTimeout(1000);
             urlConnection.connect();
 
