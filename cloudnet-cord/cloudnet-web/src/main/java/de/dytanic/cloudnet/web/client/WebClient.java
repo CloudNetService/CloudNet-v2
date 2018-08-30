20067e80b3826938fb2a8ddea0a4a514fc5361ef
Phillipp Glanz <phillipp@kallmeier-glanz.de>
2018-08-30 22:16:43 +0200

    Fix UserAgent

--- cloudnet-cord/cloudnet-web/src/main/java/de/dytanic/cloudnet/web/client/WebClient.java
+++ cloudnet-cord/cloudnet-web/src/main/java/de/dytanic/cloudnet/web/client/WebClient.java
@@ -69,6 +69,7 @@ public class WebClient {
         try
         {
             URLConnection urlConnection = new java.net.URL(url).openConnection();
+            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             urlConnection.setUseCaches(false);
             urlConnection.setConnectTimeout(1000);
             urlConnection.connect();
@@ -98,6 +99,7 @@ public class WebClient {
         try
         {
             URLConnection urlConnection = new java.net.URL(url).openConnection();
+            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             urlConnection.setUseCaches(false);
             urlConnection.setConnectTimeout(1000);
             urlConnection.connect();
