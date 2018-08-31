c9e2ff1ab156e1b296555e13a04a2a99de46fb8f
_Klaro <pasqual.koschmieder@gmail.com>
2018-08-31 12:06:33 +0200

    Issues fixed

--- cloudnet-core/src/main/java/de/dytanic/cloudnetcore/api/event/player/LoginRequestEvent.java
+++ cloudnet-core/src/main/java/de/dytanic/cloudnetcore/api/event/player/LoginRequestEvent.java
@@ -7,20 +7,18 @@ package de.dytanic.cloudnetcore.api.event.player;
 import de.dytanic.cloudnet.event.Cancelable;
 import de.dytanic.cloudnet.event.Event;
 import de.dytanic.cloudnet.lib.player.PlayerConnection;
-import lombok.*;
 
 import java.util.UUID;
 
 /**
  * Created by Tareko on 27.07.2017.
  */
-@Getter
-@AllArgsConstructor
-@ToString
+@lombok.Getter
+@lombok.AllArgsConstructor
 public class LoginRequestEvent extends Event implements Cancelable
 {
     private UUID uniqueId;
     private PlayerConnection playerConnection;
-    @Setter
+    @lombok.Setter
     private boolean cancelled = false;
 }
