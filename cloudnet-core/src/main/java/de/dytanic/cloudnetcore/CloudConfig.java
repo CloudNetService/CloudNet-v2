2ab6f3b7c6df146f83f617ed853a11827c69e875
_Klaro | Pasqual K <40468651+KlaroYT@users.noreply.github.com>
2018-10-23 13:11:31 +0200

    Update CloudConfig.java

--- cloudnet-core/src/main/java/de/dytanic/cloudnetcore/CloudConfig.java
+++ cloudnet-core/src/main/java/de/dytanic/cloudnetcore/CloudConfig.java
@@ -201,7 +201,7 @@ public class CloudConfig {
             );
             this.formatSplitter = configuration.getString("general.server-name-splitter");
             this.networkProperties = configuration.getSection("networkproperties").self;
-            //        configuration.set("general.disabled-modules", new ArrayList<>());
+            
             if (!configuration.getSection("general").self.containsKey("disabled-modules"))
             {
                 configuration.set("general.disabled-modules", new ArrayList<>());
@@ -232,8 +232,7 @@ public class CloudConfig {
         }.getType());
 
         this.userDocument = Document.loadDocument(usersPath);
-
-        /* ============================================================== */
+        
         return this;
     }
 
