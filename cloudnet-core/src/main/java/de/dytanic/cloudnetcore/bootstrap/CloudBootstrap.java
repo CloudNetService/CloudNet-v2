d981bea37a867e9ad1a1828cee27418ee2f5ac05
McRuben <roclaromi@gmail.com>
2019-08-30 15:32:13 +0200

    Removed OSSignalBlocker

--- cloudnet-core/src/main/java/de/dytanic/cloudnetcore/bootstrap/CloudBootstrap.java
+++ cloudnet-core/src/main/java/de/dytanic/cloudnetcore/bootstrap/CloudBootstrap.java
@@ -8,7 +8,6 @@ import de.dytanic.cloudnet.help.HelpService;
 import de.dytanic.cloudnet.help.ServiceDescription;
 import de.dytanic.cloudnet.lib.NetworkUtils;
 import de.dytanic.cloudnet.lib.SystemTimer;
-import de.dytanic.cloudnet.lib.utility.signal.OSSignalBlocker;
 import de.dytanic.cloudnet.logging.CloudLogger;
 import de.dytanic.cloudnet.logging.handler.ICloudLoggerHandler;
 import de.dytanic.cloudnet.logging.util.HeaderFunction;
@@ -78,9 +77,6 @@ public final class CloudBootstrap {
             System.out.println(helpService.toString());
             return;
         }
-
-        if (optionSet.has("requestTerminationSignal"))
-            OSSignalBlocker.initSignalCancel();
 
         if (optionSet.has("systemTimer"))
             new SystemTimer();
