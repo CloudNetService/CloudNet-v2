ea544799071f8c778bc68b0cc46236321e3f2512
Joschua <joschua@familie-kreimer.de>
2019-10-27 14:16:59 +0100

    Update PermissionsExample.java
    Updated Codestyle

--- cloudnet-examples/src/main/java/de/dytanic/cloudnet/PermissionsExample.java
+++ cloudnet-examples/src/main/java/de/dytanic/cloudnet/PermissionsExample.java
@@ -15,16 +15,14 @@ import org.bukkit.Bukkit;
  */
 public class PermissionsExample {
 
-    public void setTags()
-    {
+    public void setTags() {
         CloudServer.getInstance().updateNameTags(Bukkit.getPlayer("Dytanic")); //Sets the NameTags for all Players for one player. Use this method, if the scoreboard is already set
     }
 
     //More options
-    public void handlePermissions()
-    {
+    public void handlePermissions() {
         PermissionPool permissionPool = CloudAPI.getInstance().getPermissionPool(); //Returns the permission pool with all permissions groups etc.
-        PermissionGroup permissionGroup = new Boolean(true) ? CloudAPI.getInstance().getPermissionGroup("Admin") : permissionPool.getGroups().get("Admin"); //Options for got the permission group
+        PermissionGroup permissionGroup = permissionPool.getGroups().get("Admin"); //Options for got the permission group
         permissionGroup.getOptions(); //custom api options
         permissionGroup.getJoinPower(); //returns the joinpower
         permissionGroup.getPermissions(); //a map with all permissions of the group
@@ -33,7 +31,7 @@ public class PermissionsExample {
         CloudAPI.getInstance().updatePermissionGroup(permissionGroup); //Updates the permission group
     }
 
-    //Easier to use
+    //Easyer to use
     public void handlePermissions() {
         PermissionProvider.getGroupJoinPower("Admins"); //returns the joinpower of group "Admins"
         PermissionProvider.setJoinPower("Admins", 100); //set the joinpower of group "Admins" to 100
