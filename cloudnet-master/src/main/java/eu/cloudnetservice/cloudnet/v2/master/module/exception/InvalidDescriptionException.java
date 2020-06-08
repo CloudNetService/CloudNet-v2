d602658f0e3567403aab09016c45bf11da9de261
Phillipp Glanz <p.glanz@madfix.me>
2020-06-08 10:47:16 +0200

    Break a line for a bracket.

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/module/exception/InvalidDescriptionException.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/module/exception/InvalidDescriptionException.java
@@ -2,7 +2,8 @@ package eu.cloudnetservice.cloudnet.v2.master.module.exception;
 
 public final class InvalidDescriptionException extends RuntimeException {
 
-    public InvalidDescriptionException() { }
+    public InvalidDescriptionException() {
+    }
 
     public InvalidDescriptionException(final String message) {
         super(message);
