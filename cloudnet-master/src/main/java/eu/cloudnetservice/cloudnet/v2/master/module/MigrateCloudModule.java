705173c97aedfc77f4c6564818f3e96d7a87fa48
Phillipp Glanz <p.glanz@madfix.me>
2020-06-08 15:42:19 +0200

    Change from String into Semver class.

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/module/MigrateCloudModule.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/module/MigrateCloudModule.java
@@ -1,6 +1,8 @@
 package eu.cloudnetservice.cloudnet.v2.master.module;
 
+import com.vdurmont.semver4j.Semver;
+
 public interface MigrateCloudModule {
 
-    boolean migrate(String oldVersion, String newVersion);
+    boolean migrate(Semver oldVersion, Semver newVersion);
 }
