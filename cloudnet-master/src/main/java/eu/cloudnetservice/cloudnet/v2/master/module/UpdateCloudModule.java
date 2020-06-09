435161b9fc114d2e903041760d3613ae9c3e3391
Phillipp Glanz <p.glanz@madfix.me>
2020-06-09 19:24:17 +0200

    Add java docs.

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/module/UpdateCloudModule.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/module/UpdateCloudModule.java
@@ -2,5 +2,11 @@ package eu.cloudnetservice.cloudnet.v2.master.module;
 
 public interface UpdateCloudModule {
 
+    /**
+     * This method is called when an update check is performed
+     * @param url is read from the module file
+     * @return returns true if the update check was successful
+     */
     boolean update(String url);
+
 }
