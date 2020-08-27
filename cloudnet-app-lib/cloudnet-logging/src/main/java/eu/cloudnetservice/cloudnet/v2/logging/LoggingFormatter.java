b078e4302a3f31b20d4ecad3c79ffdff538cf85e
TheMeinerLP <p.glanz@madfix.me>
2020-08-27 19:34:30 +0200

    Add color support.

--- cloudnet-app-lib/cloudnet-logging/src/main/java/eu/cloudnetservice/cloudnet/v2/logging/LoggingFormatter.java
+++ cloudnet-app-lib/cloudnet-logging/src/main/java/eu/cloudnetservice/cloudnet/v2/logging/LoggingFormatter.java
@@ -1,10 +1,13 @@
 package eu.cloudnetservice.cloudnet.v2.logging;
 
+import org.fusesource.jansi.Ansi;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class LoggingFormatter extends Formatter {
@@ -15,17 +18,8 @@ public class LoggingFormatter extends Formatter {
     public String format(final LogRecord record) {
         StringBuilder builder = new StringBuilder();
         if (record != null) {
-            String message = formatMessage(record);
-            builder.append('[')
-                   .append(dateFormat.format(record.getMillis()))
-                   .append("] ")
-                   .append(record.getLevel().getName())
-                   .append(": ")
-                   .append(message);
 
-            if (!message.endsWith(System.lineSeparator())) {
-                builder.append(System.lineSeparator());
-            }
+            colorMessage(builder, record);
 
             if (record.getThrown() != null) {
                 final StringWriter stringWriter = new StringWriter();
@@ -35,5 +29,28 @@ public class LoggingFormatter extends Formatter {
 
         }
         return builder.toString();
+    }
+
+    private void colorMessage(StringBuilder builder, LogRecord record) {
+        String message = formatMessage(record);
+        builder.append('[');
+        builder.append(dateFormat.format(record.getMillis()));
+        builder.append("] ");
+        builder.append(colorLevel(record));
+        builder.append(": ");
+        builder.append(message);
+        if (!message.endsWith(System.lineSeparator())) {
+            builder.append(System.lineSeparator());
+        }
+    }
+
+    private String colorLevel(LogRecord logRecord) {
+        if (logRecord.getLevel().equals(Level.WARNING)) {
+            return Ansi.ansi().fg(Ansi.Color.YELLOW).a(logRecord.getLevel().getName()).reset().toString();
+        }
+        if (logRecord.getLevel().equals(Level.INFO)) {
+            return Ansi.ansi().fgBright(Ansi.Color.BLUE).a(logRecord.getLevel().getName()).reset().toString();
+        }
+        return logRecord.getLevel().getName();
     }
 }
