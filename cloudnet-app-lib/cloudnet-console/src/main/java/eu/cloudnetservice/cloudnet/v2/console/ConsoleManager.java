165398abdee9e792c2501af36065c965df10a246
TheMeinerLP <p.glanz@madfix.me>
2020-08-27 19:32:30 +0200

    Add own completer. Add color support. Add parser.

--- cloudnet-app-lib/cloudnet-console/src/main/java/eu/cloudnetservice/cloudnet/v2/console/ConsoleManager.java
+++ cloudnet-app-lib/cloudnet-console/src/main/java/eu/cloudnetservice/cloudnet/v2/console/ConsoleManager.java
@@ -1,27 +1,29 @@
 package eu.cloudnetservice.cloudnet.v2.console;
 
+import eu.cloudnetservice.cloudnet.v2.console.completer.CloudNetCompleter;
 import eu.cloudnetservice.cloudnet.v2.console.model.ConsoleInputDispatch;
-import eu.cloudnetservice.cloudnet.v2.console.model.LoggingOutputStream;
+import eu.cloudnetservice.cloudnet.v2.logging.LoggingOutputStream;
 import eu.cloudnetservice.cloudnet.v2.logging.CloudLogger;
 import eu.cloudnetservice.cloudnet.v2.logging.LoggingFormatter;
-import org.fusesource.jansi.AnsiConsole;
+import eu.cloudnetservice.cloudnet.v2.logging.handler.ColoredConsoleHandler;
+import org.fusesource.jansi.AnsiOutputStream;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.impl.DefaultExpander;
 import org.jline.reader.impl.DefaultHighlighter;
 import org.jline.reader.impl.DefaultParser;
 import org.jline.reader.impl.completer.ArgumentCompleter;
-import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.reader.impl.history.DefaultHistory;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
-
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 
 public final class ConsoleManager {
@@ -47,11 +49,11 @@ public final class ConsoleManager {
 
     public void useDefaultConsole() {
         //Attributes attributes = new Attributes();
-        AnsiConsole.systemInstall();
+        //AnsiConsole.systemInstall();
         Terminal terminal = null;
         try {
             terminal = TerminalBuilder.builder()
-                                           .streams(System.in, System.out)
+                                           .streams(System.in, new PrintStream(new AnsiOutputStream(System.out), true, "UTF-8"))
                                            .jansi(true)
                                            .jna(true)
                                            .encoding(StandardCharsets.UTF_8)
@@ -66,24 +68,34 @@ public final class ConsoleManager {
             this.cloudLogger.log(Level.SEVERE, "Something went wrong on creating a virtual terminal", e);
         }
         if (this.consoleInputDispatch != null && terminal != null) {
-            final ArgumentCompleter argumentCompleter = new ArgumentCompleter(new StringsCompleter(this.consoleInputDispatch.get()));
+            final ArgumentCompleter argumentCompleter = new ArgumentCompleter(new CloudNetCompleter(this.consoleInputDispatch));
             argumentCompleter.setStrict(false);
             argumentCompleter.setStrictCommand(false);
+            final DefaultParser defaultParser = new DefaultParser();
+            defaultParser.setEofOnUnclosedBracket(DefaultParser.Bracket.ANGLE, DefaultParser.Bracket.CURLY, DefaultParser.Bracket.ROUND, DefaultParser.Bracket.SQUARE);
             this.lineReader = LineReaderBuilder.builder()
                                                .appName("CloudNet-Console")
                                                .option(LineReader.Option.ERASE_LINE_ON_FINISH, true)
                                                .option(LineReader.Option.INSERT_BRACKET, true)
+                                               .option(LineReader.Option.GROUP, true)
+                                               .option(LineReader.Option.LIST_PACKED, true)
                                                .highlighter(new DefaultHighlighter())
                                                .expander(new DefaultExpander())
                                                .history(new DefaultHistory())
                                                .variable(LineReader.HISTORY_FILE, Paths.get(".cn_history"))
                                                .terminal(terminal)
-                                               .parser(new DefaultParser())
+                                               .parser(defaultParser)
                                                .completer(argumentCompleter)
                                                .build();
+            lineReader.setAutosuggestion(LineReader.SuggestionType.COMPLETER);
         } else {
             System.exit(-1);
             throw new NullPointerException("Console input dispatcher is empty");
+        }
+        for (final Handler handler : this.cloudLogger.getHandlers()) {
+            if (handler instanceof ColoredConsoleHandler) {
+                this.cloudLogger.removeHandler(handler);
+            }
         }
         final JLine3ConsoleHandler jLine3ConsoleHandler = new JLine3ConsoleHandler(this.lineReader);
         jLine3ConsoleHandler.setLevel(Level.INFO);
@@ -91,8 +103,7 @@ public final class ConsoleManager {
         try {
             jLine3ConsoleHandler.setEncoding(StandardCharsets.UTF_8.name());
             this.cloudLogger.addHandler(jLine3ConsoleHandler);
-            System.setOut(AnsiConsole.wrapSystemOut(new PrintStream(new LoggingOutputStream(this.cloudLogger, Level.INFO), true, StandardCharsets.UTF_8.name())));
-            System.setErr(AnsiConsole.wrapSystemOut(new PrintStream(new LoggingOutputStream(this.cloudLogger, Level.SEVERE), true, StandardCharsets.UTF_8.name())));
+
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
--- cloudnet-app-lib/cloudnet-console/src/main/java/eu/cloudnetservice/cloudnet/v2/console/JLine3ConsoleHandler.java
+++ cloudnet-app-lib/cloudnet-console/src/main/java/eu/cloudnetservice/cloudnet/v2/console/JLine3ConsoleHandler.java
@@ -1,9 +1,12 @@
 package eu.cloudnetservice.cloudnet.v2.console;
 
+import eu.cloudnetservice.cloudnet.v2.logging.color.ChatColor;
+import org.fusesource.jansi.Ansi;
 import org.jline.reader.LineReader;
 
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
+import java.util.regex.Pattern;
 
 public class JLine3ConsoleHandler extends Handler {
 
@@ -21,7 +24,12 @@ public class JLine3ConsoleHandler extends Handler {
             return;
         }
         if (isLoggable(record)) {
-            lineReader.printAbove(getFormatter().format(record));
+            String s = getFormatter().format(record);
+            for (ReplacementSpecification replacement : REPLACEMENTS) {
+                s = replacement.pattern.matcher(s).replaceAll(replacement.replacement);
+            }
+            s = s + Ansi.ansi().a(Ansi.Attribute.RESET).toString();
+            lineReader.printAbove(s);
         }
     }
 
@@ -34,4 +42,45 @@ public class JLine3ConsoleHandler extends Handler {
     public void close() throws SecurityException {
         closed = true;
     }
+
+    private static class ReplacementSpecification {
+
+        private final Pattern pattern;
+        private final String replacement;
+
+        private ReplacementSpecification(final Pattern pattern, final String replacement) {
+            this.pattern = pattern;
+            this.replacement = replacement;
+        }
+    }
+
+    private static ReplacementSpecification compile(ChatColor color, String ansi) {
+        return new ReplacementSpecification(Pattern.compile("(?i)" + color.toString()), ansi);
+    }
+
+    private static final ReplacementSpecification[] REPLACEMENTS = new ReplacementSpecification[]
+        {
+            compile(ChatColor.BLACK, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).boldOff().toString()),
+            compile(ChatColor.DARK_BLUE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).boldOff().toString()),
+            compile(ChatColor.DARK_GREEN, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).boldOff().toString()),
+            compile(ChatColor.DARK_AQUA, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).boldOff().toString()),
+            compile(ChatColor.DARK_RED, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).boldOff().toString()),
+            compile(ChatColor.DARK_PURPLE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).boldOff().toString()),
+            compile(ChatColor.GOLD, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).boldOff().toString()),
+            compile(ChatColor.GRAY, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).boldOff().toString()),
+            compile(ChatColor.DARK_GRAY, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).bold().toString()),
+            compile(ChatColor.BLUE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).bold().toString()),
+            compile(ChatColor.GREEN, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).bold().toString()),
+            compile(ChatColor.AQUA, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).bold().toString()),
+            compile(ChatColor.RED, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).bold().toString()),
+            compile(ChatColor.LIGHT_PURPLE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).bold().toString()),
+            compile(ChatColor.YELLOW, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).bold().toString()),
+            compile(ChatColor.WHITE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).bold().toString()),
+            compile(ChatColor.MAGIC, Ansi.ansi().a(Ansi.Attribute.BLINK_SLOW).toString()),
+            compile(ChatColor.BOLD, Ansi.ansi().a(Ansi.Attribute.UNDERLINE_DOUBLE).toString()),
+            compile(ChatColor.STRIKETHROUGH, Ansi.ansi().a(Ansi.Attribute.STRIKETHROUGH_ON).toString()),
+            compile(ChatColor.UNDERLINE, Ansi.ansi().a(Ansi.Attribute.UNDERLINE).toString()),
+            compile(ChatColor.ITALIC, Ansi.ansi().a(Ansi.Attribute.ITALIC).toString()),
+            compile(ChatColor.RESET, Ansi.ansi().a(Ansi.Attribute.RESET).toString()),
+        };
 }
