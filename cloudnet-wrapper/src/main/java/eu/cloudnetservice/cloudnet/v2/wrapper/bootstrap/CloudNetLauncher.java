695607994fd59ecc26397cffdfb6a00bd2ca3dca
TheMeinerLP <p.glanz@madfix.me>
2020-08-28 18:53:21 +0200

    Add ansi support.

--- cloudnet-wrapper/src/main/java/eu/cloudnetservice/cloudnet/v2/wrapper/bootstrap/CloudNetLauncher.java
+++ cloudnet-wrapper/src/main/java/eu/cloudnetservice/cloudnet/v2/wrapper/bootstrap/CloudNetLauncher.java
@@ -1,16 +1,15 @@
 package eu.cloudnetservice.cloudnet.v2.wrapper.bootstrap;
 
-/**
- * Created by Tareko on 18.09.2017.
- */
+import org.fusesource.jansi.AnsiConsole;
+
 public class CloudNetLauncher {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         if (Float.parseFloat(System.getProperty("java.class.version")) < 52D) {
             System.out.println("This application needs Java 8 or 10.0.1");
             return;
         }
-
+        AnsiConsole.systemInstall();
         CloudBootstrap.main(args);
 
     }
