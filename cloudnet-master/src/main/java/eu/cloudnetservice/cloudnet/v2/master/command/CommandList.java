5f890a69ab0b309c682d605e00d7f8b9f1db9e85
TheMeinerLP <p.glanz@madfix.me>
2021-03-01 14:11:38 +0100

    Improve list command

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandList.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandList.java
@@ -22,48 +22,60 @@ public final class CommandList extends Command {
     @Override
     public void onExecuteCommand(CommandSender sender, ParsedLine parsedLine) {
         sender.sendMessage("CloudNet: ");
+        sender.sendMessage("Wrappers: ");
         int memory = 0, maxMemory = 0;
         for (Wrapper wrapper : CloudNet.getInstance().getWrappers().values()) {
             memory += wrapper.getUsedMemory();
             maxMemory += wrapper.getMaxMemory();
-
-            sender.sendMessage("Wrapper: [" + wrapper.getServerId() + "] @" + wrapper.getNetworkInfo().getHostName());
-            sender.sendMessage("Info: CPU Usage: " + NetworkUtils.DECIMAL_FORMAT.format(wrapper.getCpuUsage()) + "/100% | Memory: " + wrapper
-                .getUsedMemory() + NetworkUtils.SLASH_STRING + wrapper.getMaxMemory() + "MB");
-            if (wrapper.getWrapperInfo() != null) {
-                sender.sendMessage("CloudNet-Wrapper Version: " + wrapper.getWrapperInfo().getVersion());
-            }
-
+            String stringBuilder = " - " + wrapper.getServerId() + "@" + wrapper.getNetworkInfo()
+                                                                                .getHostName() + "(" + colorizeUsage((int) wrapper.getCpuUsage(), 100) + "% | Memory: " + colorizeUsage(wrapper.getUsedMemory(),
+                                                                            wrapper.getMaxMemory()) + "MB | " + (wrapper.getWrapperInfo() != null ? wrapper
+                .getWrapperInfo()
+                .getVersion() : "Unknown") + ")";
+            sender.sendMessage(stringBuilder);
             sender.sendMessage(" ", "Proxys:");
             for (ProxyServer proxyServer : wrapper.getProxies().values()) {
-                sender.sendMessage("Proxy [" + proxyServer.getServerId() + "] @" + proxyServer.getNetworkInfo()
-                                                                                              .getHostName() + " | " + proxyServer.getProxyInfo()
-                                                                                                                                  .getOnlineCount() + NetworkUtils.SLASH_STRING + CloudNet
+                sender.sendMessage(" - " + proxyServer.getServerId() + "@" + proxyServer.getNetworkInfo()
+                                                                                              .getHostName() + " | " + colorizeUsage(proxyServer.getProxyInfo()
+                                                                                                                                  .getOnlineCount(), CloudNet
                     .getInstance()
                     .getProxyGroups()
                     .get(proxyServer.getProxyInfo().getServiceId().getGroup())
                     .getProxyConfig()
-                    .getMaxPlayers() + " | State: " + (proxyServer.getChannel() != null ? "connected" : "not connected"));
+                    .getMaxPlayers()) + " | State: " + (proxyServer.getChannel() != null ? "connected" : "not connected"));
             }
             sender.sendMessage(" ", "Servers:");
-            for (MinecraftServer proxyServer : wrapper.getServers().values()) {
-                sender.sendMessage("Server [" + proxyServer.getServerId() + "] @" + proxyServer.getServerInfo()
-                                                                                               .getHost() + " | " + proxyServer.getServerInfo()
-                                                                                                                               .getOnlineCount() + NetworkUtils.SLASH_STRING + proxyServer
+            for (MinecraftServer minecraftServer : wrapper.getServers().values()) {
+                sender.sendMessage(" - " + minecraftServer.getServerId() + "@" + minecraftServer.getServerInfo()
+                                                                                               .getHost() + " | " + colorizeUsage(minecraftServer.getServerInfo()
+                                                                                                                                             .getOnlineCount(), minecraftServer
                     .getServerInfo()
-                    .getMaxPlayers() + " | State: " + (proxyServer.getChannel() != null ? "connected" : "not connected"));
+                    .getMaxPlayers()) + " | State: " + (minecraftServer.getChannel() != null ? "connected" : "not connected"));
             }
             sender.sendMessage(" ");
         }
-
+        sender.sendMessage(" ", "Online Players:");
         for (CloudPlayer cloudPlayer : CloudNet.getInstance().getNetworkManager().getOnlinePlayers().values()) {
-            sender.sendMessage("* " + cloudPlayer.getUniqueId() + '#' + cloudPlayer.getName() + " - " + cloudPlayer.getProxy() + ':' + cloudPlayer
+            sender.sendMessage("* " + cloudPlayer.getUniqueId() + '#' + cloudPlayer.getName() + "@" + cloudPlayer.getProxy() + '>' + cloudPlayer
                 .getServer());
         }
 
         sender.sendMessage(" ");
 
-        sender.sendMessage("CloudNet uses " + memory + NetworkUtils.SLASH_STRING + maxMemory + "MB ");
+        sender.sendMessage("CloudNet uses " + colorizeUsage(memory, maxMemory) + "MB ");
 
+    }
+
+    private String colorizeUsage(int used, int max) {
+        int onePer = max / 100;
+        int fivePer = onePer * 50;
+        int eightPer = onePer * 80;
+
+        if (used >= fivePer && used <= eightPer) {
+            return "§e" + used + "§r" + NetworkUtils.SLASH_STRING + max;
+        } else if (used >= fivePer && used >= eightPer) {
+            return "§c" + used + "§r" + NetworkUtils.SLASH_STRING + max;
+        }
+        return "§a" + used + "§r" + NetworkUtils.SLASH_STRING + max;
     }
 }
