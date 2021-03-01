858fa322da388db3ea643e54bf26850f0884e312
TheMeinerLP <p.glanz@madfix.me>
2021-03-01 17:19:59 +0100

    Improve help command

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandHelp.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandHelp.java
@@ -12,9 +12,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by Tareko on 26.07.2017.
- */
 public class CommandHelp extends Command {
 
     public static final String[] EMPTY_STRING_ARRAY = new String[0];
@@ -30,18 +27,6 @@ public class CommandHelp extends Command {
         for (String command : CloudNet.getInstance().getCommandManager().getCommands()) {
             messages.add(command + " | " + CloudNet.getInstance().getCommandManager().getCommand(command).getDescription());
         }
-
-        messages.add(NetworkUtils.SPACE_STRING);
-        messages.add("Server groups:");
-        messages.add(Arrays.toString(CloudNet.getInstance().getServerGroups().keySet().toArray(EMPTY_STRING_ARRAY)));
-        messages.add("Proxy groups:");
-        messages.add(Arrays.toString(CloudNet.getInstance().getProxyGroups().keySet().toArray(EMPTY_STRING_ARRAY)));
-        messages.add(NetworkUtils.SPACE_STRING);
-        messages.add(String.format("The Cloud uses %d/%dMB",
-                                   ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1048576L,
-                                   ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax() / 1048576L));
-        messages.add("CPU on this instance " + new DecimalFormat("##.##").format(NetworkUtils.internalCpuUsage()) + "/100 %");
-        messages.add(NetworkUtils.SPACE_STRING);
 
         sender.sendMessage(messages.toArray(EMPTY_STRING_ARRAY));
     }
