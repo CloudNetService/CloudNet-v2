bac288e6debdd44f3ce624df2eef31a42f48714a
TheMeinerLP <p.glanz@madfix.me>
2021-03-01 11:33:13 +0100

    Rewrite reload command

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandReload.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandReload.java
@@ -2,11 +2,16 @@ package eu.cloudnetservice.cloudnet.v2.master.command;
 
 import eu.cloudnetservice.cloudnet.v2.command.Command;
 import eu.cloudnetservice.cloudnet.v2.command.CommandSender;
+import eu.cloudnetservice.cloudnet.v2.command.TabCompletable;
 import eu.cloudnetservice.cloudnet.v2.master.CloudNet;
 import eu.cloudnetservice.cloudnet.v2.master.network.components.Wrapper;
+import org.jline.reader.Candidate;
 import org.jline.reader.ParsedLine;
 
-public final class CommandReload extends Command {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class CommandReload extends Command implements TabCompletable {
 
     public CommandReload() {
         super("reload", "cloudnet.command.reload", "rl");
@@ -18,22 +23,23 @@ public final class CommandReload extends Command {
     @Override
     public void onExecuteCommand(CommandSender sender, ParsedLine parsedLine) {
         if (parsedLine.words().size() == 2) {
-            if (parsedLine.words().get(1).equalsIgnoreCase("all")) {
-                sender.sendMessage("[RELOAD] Trying to reload CloudNet...");
+            String commandArgument = parsedLine.words().get(1);
+            if (commandArgument.equalsIgnoreCase("all")) {
+                sender.sendMessage("§e[RELOAD] Trying to reload CloudNet...");
                 try {
                     CloudNet.getInstance().reload();
-                    sender.sendMessage("[RELOAD] Reloading was completed successfully!");
+                    sender.sendMessage("§a[RELOAD] Reloading was completed successfully!");
                 } catch (Exception e) {
-                    sender.sendMessage("[RELOAD] Failed to reload CloudNet");
+                    sender.sendMessage("§c[RELOAD] Failed to reload CloudNet");
                     e.printStackTrace();
                 }
             }
-            if (parsedLine.words().get(1).equalsIgnoreCase("config")) {
-                sender.sendMessage("[RELOAD] Trying to reload config");
+            if (commandArgument.equalsIgnoreCase("config")) {
+                sender.sendMessage("§e[RELOAD] Trying to reload config");
                 reloadConfig();
-                sender.sendMessage("[RELOAD] Reloading was completed successfully");
+                sender.sendMessage("§a[RELOAD] Reloading was completed successfully");
             }
-            if (parsedLine.words().get(1).equalsIgnoreCase("wrapper")) {
+            if (commandArgument.equalsIgnoreCase("wrapper")) {
                 for (Wrapper wrapper : CloudNet.getInstance().getWrappers().values()) {
                     if (wrapper.getChannel() != null) {
                         wrapper.writeCommand("reload");
@@ -60,18 +66,32 @@ public final class CommandReload extends Command {
 
         CloudNet.getInstance().getServerGroups().putAll(CloudNet.getInstance().getConfig().getServerGroups());
         CloudNet.getInstance().getServerGroups().forEach((name, serverGroup) -> {
-            CloudNet.getLogger().info(String.format("Loading server group: %s%n", serverGroup.getName()));
+            CloudNet.getLogger().info(String.format("Loading server group: %s", serverGroup.getName()));
             CloudNet.getInstance().setupGroup(serverGroup);
         });
 
         CloudNet.getInstance().getProxyGroups().putAll(CloudNet.getInstance().getConfig().getProxyGroups());
         CloudNet.getInstance().getProxyGroups().forEach((name, proxyGroup) -> {
-            CloudNet.getLogger().info(String.format("Loading proxy group: %s%n", proxyGroup.getName()));
+            CloudNet.getLogger().info(String.format("Loading proxy group: %s", proxyGroup.getName()));
             CloudNet.getInstance().setupProxy(proxyGroup);
         });
 
         CloudNet.getInstance().getNetworkManager().reload();
         CloudNet.getInstance().getNetworkManager().updateAll();
         CloudNet.getInstance().getWrappers().values().forEach(Wrapper::updateWrapper);
+    }
+
+    @Override
+    public List<Candidate> onTab(ParsedLine parsedLine) {
+        List<Candidate> strings = new ArrayList<>();
+        if (parsedLine.words().size() >= 1) {
+            String commandArgument = parsedLine.words().get(0);
+            if (commandArgument.equalsIgnoreCase("reload") || commandArgument.equalsIgnoreCase("rl")) {
+                strings.add(new Candidate("ALL", "ALL", null, "Loads all groups as well as modules, permissions, etc.", null ,null, true));
+                strings.add(new Candidate("CONFIG", "CONFIG", null, "Reload the configuration file, and its server groups etc.", null ,null, true));
+                strings.add(new Candidate("WRAPPER", "WRAPPER", null, "Dispatched on all wrappers the command \"reload\"", null ,null, true));
+            }
+        }
+        return strings;
     }
 }
