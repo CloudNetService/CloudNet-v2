b4eb3fc5638103fa85b166e7b25bbef74daf813e
TheMeinerLP <p.glanz@madfix.me>
2021-03-01 10:39:28 +0100

    Make class final

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandWrapper.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandWrapper.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class CommandWrapper extends Command implements TabCompletable {
+public final class CommandWrapper extends Command implements TabCompletable {
 
     /**
      * Constructs a new command with a name, a needed permission and variable aliases.
