ce27c40aa3814e98b8d5548ad48e773dba6a307c
TheMeinerLP <p.glanz@madfix.me>
2021-03-01 11:32:51 +0100

    Remove new line

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/CloudNet.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/CloudNet.java
@@ -631,13 +631,13 @@ public final class CloudNet extends EventKey implements Executable, Reloadable {
 
         this.serverGroups.putAll(config.getServerGroups());
         this.serverGroups.forEach((name, serverGroup) -> {
-            logger.info(String.format("Loading server group: %s%n", serverGroup.getName()));
+            logger.info(String.format("Loading server group: %s", serverGroup.getName()));
             setupGroup(serverGroup);
         });
 
         this.proxyGroups.putAll(config.getProxyGroups());
         this.proxyGroups.forEach((name, proxyGroup) -> {
-            logger.info(String.format("Loading proxy group: %s%n", proxyGroup.getName()));
+            logger.info(String.format("Loading proxy group: %s", proxyGroup.getName()));
             setupProxy(proxyGroup);
         });
 
