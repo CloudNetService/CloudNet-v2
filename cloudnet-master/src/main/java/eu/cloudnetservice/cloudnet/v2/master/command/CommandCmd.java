f3ed1e457c61af1820082b526488c379ad8a2d37
TheMeinerLP <p.glanz@madfix.me>
2021-03-02 07:44:12 +0100

    Improve cmd command

--- cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandCmd.java
+++ cloudnet-master/src/main/java/eu/cloudnetservice/cloudnet/v2/master/command/CommandCmd.java
@@ -2,17 +2,19 @@ package eu.cloudnetservice.cloudnet.v2.master.command;
 
 import eu.cloudnetservice.cloudnet.v2.command.Command;
 import eu.cloudnetservice.cloudnet.v2.command.CommandSender;
+import eu.cloudnetservice.cloudnet.v2.command.TabCompletable;
 import eu.cloudnetservice.cloudnet.v2.lib.NetworkUtils;
 import eu.cloudnetservice.cloudnet.v2.master.CloudNet;
 import eu.cloudnetservice.cloudnet.v2.master.network.components.MinecraftServer;
 import eu.cloudnetservice.cloudnet.v2.master.network.components.ProxyServer;
 import eu.cloudnetservice.cloudnet.v2.master.network.components.Wrapper;
+import org.jline.reader.Candidate;
 import org.jline.reader.ParsedLine;
 
-/**
- * Created by Tareko on 20.08.2017.
- */
-public final class CommandCmd extends Command {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class CommandCmd extends Command implements TabCompletable {
 
     public CommandCmd() {
         super("cmd", "cloudnet.command.cmd", "command");
@@ -24,30 +26,30 @@ public final class CommandCmd extends Command {
     public void onExecuteCommand(CommandSender sender, ParsedLine parsedLine) {
         if (parsedLine.words().size() > 1) {
             StringBuilder stringBuilder = new StringBuilder();
-            for (short i = 1; i < parsedLine.words().size(); i++) {
+            for (short i = 2; i < parsedLine.words().size(); i++) {
                 stringBuilder.append(parsedLine.words().get(i)).append(NetworkUtils.SPACE_STRING);
             }
             String command = stringBuilder.substring(0, stringBuilder.length() - 1);
             for (Wrapper wrapper : CloudNet.getInstance().getWrappers().values()) {
-                if (wrapper.getName().equalsIgnoreCase(parsedLine.words().get(0))) {
+                if (wrapper.getName().equalsIgnoreCase(parsedLine.words().get(1))) {
                     wrapper.writeCommand(command);
                     sender.sendMessage("Sending command to " + wrapper.getName() + " with [\"" + command + "\"]");
                     return;
                 }
 
                 for (MinecraftServer minecraftServer : wrapper.getServers().values()) {
-                    if (minecraftServer.getServiceId().getServerId().equalsIgnoreCase(parsedLine.words().get(0))) {
+                    if (minecraftServer.getServiceId().getServerId().equalsIgnoreCase(parsedLine.words().get(1))) {
                         minecraftServer.getWrapper().writeServerCommand(command, minecraftServer.getServerInfo());
-                        sender.sendMessage("Sending command to " + minecraftServer.getServiceId()
+                        sender.sendMessage("§aSending command to " + minecraftServer.getServiceId()
                                                                                   .getServerId() + " with [\"" + command + "\"]");
                         return;
                     }
                 }
 
                 for (ProxyServer minecraftServer : wrapper.getProxies().values()) {
-                    if (minecraftServer.getServiceId().getServerId().equalsIgnoreCase(parsedLine.words().get(0))) {
+                    if (minecraftServer.getServiceId().getServerId().equalsIgnoreCase(parsedLine.words().get(1))) {
                         minecraftServer.getWrapper().writeProxyCommand(command, minecraftServer.getProxyInfo());
-                        sender.sendMessage("Sending command to " + minecraftServer.getServiceId()
+                        sender.sendMessage("§aSending command to " + minecraftServer.getServiceId()
                                                                                   .getServerId() + " with [\"" + command + "\"]");
                         return;
                     }
@@ -56,5 +58,17 @@ public final class CommandCmd extends Command {
         } else {
             sender.sendMessage("cmd <name> <command> | Executes a command, either from a wrapper, proxy or game server");
         }
+    }
+
+    @Override
+    public List<Candidate> onTab(ParsedLine parsedLine) {
+        List<Candidate> candidates = new ArrayList<>();
+        for (MinecraftServer minecraftServer : CloudNet.getInstance().getServers().values()) {
+            candidates.add(new Candidate(minecraftServer.getName(), minecraftServer.getName(), minecraftServer.getGroup().getName(), "A minecraft server", null ,null, true));
+        }
+        for (ProxyServer proxyServer : CloudNet.getInstance().getProxys().values()) {
+            candidates.add(new Candidate(proxyServer.getName(), proxyServer.getName(), proxyServer.getProcessMeta().getProxyGroupName(), "A proxy", null ,null, true));
+        }
+        return candidates;
     }
 }
